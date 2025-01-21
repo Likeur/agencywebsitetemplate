088ccd6e2093546436839c6c153c377ee3ecb6ef
Likeur <jacksimba28@gmail.com>
2025-01-21 14:11:41 +0200

    add project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        title: 'Agency website template',
+        loadComponent: () => import('./layout/landingpage/landingpage.component')
+    }
+];
---
+++ src/app/layout/landingpage/landingpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HerosectionComponent } from '../../components/herosection/herosection.component';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [HerosectionComponent],
+  templateUrl: './landingpage.component.html',
+})
+export default class LandingpageComponent {
+
+}
