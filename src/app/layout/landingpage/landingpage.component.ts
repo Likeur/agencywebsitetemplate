b434dd503654b3e22eaf73b99fc9f0e383e18416
Likeur <jacksimba28@gmail.com>
2025-01-21 16:14:27 +0200

    add revealanimation for testimonials

--- src/app/layout/landingpage/landingpage.component.ts
+++ src/app/layout/landingpage/landingpage.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { HerosectionComponent } from '../../components/herosection/herosection.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CtaComponent } from '../../components/cta/cta.component';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-landingpage',
-  imports: [HerosectionComponent],
+  imports: [HerosectionComponent, TestimonialsComponent, CtaComponent, FooterComponent],
   templateUrl: './landingpage.component.html',
 })
-export default class LandingpageComponent {
+export default class LandingpageComponent implements AfterViewInit {
 
+
+  public ngAfterViewInit(): void {
+    
+    gsap.from('#testiheadline1, #testiheadline2, #testibox,  .onetesti', {
+      scrollTrigger: {
+        trigger: '#testisection',
+        start: 'top 50%', 
+        end: 'bottom 50%',    
+        toggleActions: 'play none none reverse',
+      },
+      opacity: 0,
+      y:50,
+      stagger:{
+        amount:0.2
+      },
+      ease:'power4.inOut',
+      duration: 1,
+      
+    });
+    
+  }
 }
