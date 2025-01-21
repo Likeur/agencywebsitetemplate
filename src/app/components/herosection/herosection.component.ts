16b3ad4d741649543c126676133ffe2905649246
Likeur <jacksimba28@gmail.com>
2025-01-21 15:30:32 +0200

    add hero section overlay animation

--- src/app/components/herosection/herosection.component.ts
+++ src/app/components/herosection/herosection.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-herosection',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './herosection.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HerosectionComponent {
+export class HerosectionComponent implements AfterViewInit {
+  public overlaytl = gsap.timeline({});
 
+  public ngAfterViewInit(): void {
+    gsap.to('.overlay1', {
+      x: 400,
+      opacity: 0.1,
+      y: -170,
+      bottom:2,
+      duration: 11,
+
+      repeat: -1, // Répète indéfiniment
+      yoyo: true,
+      ease: 'power4.inOut',
+    });
+
+    gsap.to('.overlay2', {
+      x: -800,
+      y: -220,
+      opacity: 0.1,
+      duration: 9,
+
+      repeat: -1, // Répète indéfiniment
+      yoyo: true,
+      ease: 'ease',
+    });
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NavbarComponent {
-
+  public linkStyle = signal("hover:text-orange-600 duration-300")
 }
