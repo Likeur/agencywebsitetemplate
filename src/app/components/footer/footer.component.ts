992e545c2974754d004b6e8538f8123c2ab9395b
Likeur <jacksimba28@gmail.com>
2025-01-21 16:39:46 +0200

    add cta section ft footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styles: ``
+  styles: ``,
 })
 export class FooterComponent {
-
+  public linkStylefooter = signal('hover:text-orange-600 duration-300');
 }
