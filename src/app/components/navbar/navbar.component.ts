ae6bf84bf85a690d5dd090251c7134e9419fc5c1
Likeur <jacksimba28@gmail.com>
2025-01-31 23:57:24 +0200

    set responsive nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styles: ``
 })
 export class NavbarComponent {
+  public navIsOpen = false
   public linkStyle = signal("hover:text-orange-600 duration-300")
+
+
+  public toggleNav(){
+    this.navIsOpen = !this.navIsOpen
+  }
 }
